a6afdc344010d567ecac824234a5e31457247a72
Orobinsk <104093880+Orobinsk@users.noreply.github.com>
2023-06-22 17:14:34 +0300

    add config

---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import classes from './Counter.module.scss'
+
+const Counter = () => {
+
+    const[count,setCount]=useState(0)
+
+    return (
+        <div className={classes.btn}>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(count+1)} >+1</button>
+        </div>
+    );
+};
+
+export default Counter;
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            my ABOUT page
+        </div>
+    );
+};
+
+export default AboutPage;
