7bd53393b57f0bccaf4ca7e676d9acdcf092bc4b
Orobinsk <104093880+Orobinsk@users.noreply.github.com>
2023-06-23 18:50:16 +0300

    navbar and themes

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+
+
+export enum AppRoute{
+    MAIN='main',
+    ABOUT='about',
+}
+
+export const RoutePatch: Record<AppRoute, string>={
+    [AppRoute.MAIN]: '/',
+    [AppRoute.ABOUT]:'/about',
+}
+
+export const routeConfig: Record<AppRoute, RouteProps>={
+    [AppRoute.MAIN]:{
+        path: RoutePatch.main,
+        element: <MainPage/>
+    },
+    [AppRoute.ABOUT]:{
+        path: RoutePatch.about,
+        element: <AboutPage/>
+    }
+}
