4daa2abf6338eccf67f15f69b12ff3d466ac61ef
Orobinsk <104093880+Orobinsk@users.noreply.github.com>
2023-06-26 23:04:23 +0300

    sidebar and langSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,16 +5,22 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/NavBar";
+import {Sidebar} from "widgets/Sidebar";
+
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <AppRouter/>
-            <button onClick={toggleTheme}>TOGGLE</button>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className='content-page'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
