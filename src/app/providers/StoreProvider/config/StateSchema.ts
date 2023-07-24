23fa3dbefa8ee8ef981e12c1b70fb20664d35be7
Orobinsk <104093880+Orobinsk@users.noreply.github.com>
2023-07-24 12:11:51 +0300

    add new input, add login form ui

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,5 +1,7 @@
 import { CounterSchema } from 'entities/Counters';
+import { UserSchema } from 'entities/User';
 
 export interface StateSchema {
     counter:CounterSchema;
+    user: UserSchema
 }
--- src/widgets/NavBar/ui/Navbar.tsx
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -3,31 +3,37 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps{
     className?:string;
+
 }
 
 export const Navbar = ({ className }:NavbarProps) => {
     const { t } = useTranslation();
-    const [isAuthModal, setisAuthModal] = useState(false);
-    const onToggleModal = useCallback(() => {
-        setisAuthModal((prev) => !prev);
+    const [isAuthModal, setIsAuthModal] = useState(false);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <Button
                 theme={ButtonTheme.CLEAR_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
             {/* eslint-disable-next-line i18next/no-literal-string */}
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                ghj,ysq ntrcn11111
-            </Modal>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
 
     );
